bc10415f1d8db2fa796a57d56c6b4d5341e13bae
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-07-23 19:24:01 +0200

    initial Experiment 4

---
+++ Iota_Experiment_4/src/main/java/com/example/iota_experiment_4/IotaExperiment4Application.java
@@ -0,0 +1,13 @@
+package com.example.iota_experiment_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotaExperiment4Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(IotaExperiment4Application.class, args);
+  }
+
+}
---
+++ Iota_Experiment_4/src/test/java/com/example/iota_experiment_4/IotaExperiment4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iota_experiment_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotaExperiment4ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
