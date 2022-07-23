b10d9d72ab498ea5e409d45eb0e867118880b7bb
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-07-23 19:19:56 +0200

    import wird nicht gefunden, obwohl es da ist

--- Iota_Experiment_1/src/main/java/com/example/iota_experiment_1/controllers/WebController.java
+++ Iota_Experiment_1/src/main/java/com/example/iota_experiment_1/controllers/WebController.java
@@ -1,22 +1,22 @@
 package com.example.iota_experiment_1.controllers;
 
-//import org.iota.Client;
-//import org.iota.types.ClientConfig;
-//import org.iota.types.ClientException;
-//import org.iota.types.responses.NodeInfoResponse;
+import org.iota.Client;
+import org.iota.types.ClientConfig;
+import org.iota.types.ClientException;
+import org.iota.types.responses.NodeInfoResponse;
 
 public class WebController {
 
-  public static void main(String[] args) {
-//    Client client = new Client(new ClientConfig("{ \"nodes\": [ \"https://api.testnet.shimmer.network\" ], \"nodeSyncEnabled\": true }"));
-////
-////    // Get the node information for a given node.
-//    NodeInfoResponse response = client.getNodeInfo();
-////
-////    // Print the URL of the node that was requested.
-//    System.out.println(response.getNodeUrl());
-////
-////    // Print the node information for the requested node.
-//    System.out.println(response.getNodeInfo());
+  public static void main(String[] args) throws ClientException {
+    Client client = new Client(new ClientConfig("{ \"nodes\": [ \"https://api.testnet.shimmer.network\" ], \"nodeSyncEnabled\": true }"));
+//
+//    // Get the node information for a given node.
+    NodeInfoResponse response = client.getNodeInfo();
+//
+//    // Print the URL of the node that was requested.
+    System.out.println(response.getNodeUrl());
+//
+//    // Print the node information for the requested node.
+    System.out.println(response.getNodeInfo());
   }
 }
