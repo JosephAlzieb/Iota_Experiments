bbed1959770299a586d4f18bea20539db5712249
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-07-23 19:19:04 +0200

    der Node läuft nicht

---
+++ Iota_Experiment_3/src/main/java/com/example/iota_experiment_3/Main.java
@@ -0,0 +1,60 @@
+package com.example.iota_experiment_3;
+
+import java.util.ArrayList;
+import org.iota.jota.IotaAPI;
+import org.iota.jota.dto.response.SendTransferResponse;
+import org.iota.jota.error.ArgumentException;
+import org.iota.jota.model.Transfer;
+import org.iota.jota.utils.SeedRandomGenerator;
+import org.iota.jota.utils.TrytesConverter;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    IotaAPI api = new IotaAPI.Builder()
+        .protocol("https")
+        .host("chrysalis-nodes.iota.org")
+        .port(443)
+        .build();
+    int depth = 3;
+    int minimumWeightMagnitude = 9;
+
+    // Even though a seed is not necessary because zero value transactions are not signed,
+    // the library requires a seed to send a transaction.
+    // This seed can be any random string of 81 trytes
+    String myRandomSeed = SeedRandomGenerator.generateNewSeed();
+
+    // Define any security level (like the seed, this is not used)
+    int securityLevel = 2;
+
+    // Define an address.
+    // This does not need to belong to anyone or have IOTA tokens.
+    // It must only contain a maximum of 81 trytes
+    // or 90 trytes with a valid checksum
+    String address = "ZLGVEQ9JUZZWCZXLWVNTHBDX9G9KZTJP9VEERIIFHY9SIQKYBVAHIMLHXPQVE9IXFDDXNHQINXJDRPFDXNYVAPLZAW";
+    // This is a zero-value transaction
+    int value = 0;
+    // Define a message to send.
+    // This message must include only ASCII characters.
+    String message = TrytesConverter.asciiToTrytes("Hello world");
+    String tag = "HELLOWORLD";
+
+    Transfer zeroValueTransaction = new Transfer(address, value, message, tag);
+
+    ArrayList<Transfer> transfers = new ArrayList<>();
+
+    transfers.add(zeroValueTransaction);
+
+    // Create a bundle from the transfers list
+    // and send the transaction to the node
+    try {
+      // Since we don't send any value, we can skip validation of inputs
+      SendTransferResponse response = api.sendTransfer(myRandomSeed, securityLevel, depth, minimumWeightMagnitude, transfers, null, null, false, false, null);
+      System.out.println(response.getTransactions());
+    } catch (ArgumentException e) {
+      // Handle error
+      e.printStackTrace();
+    }
+  }
+}
